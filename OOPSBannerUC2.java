7bc95b6cdf4d2ff0048c54321216c54520f7dad1
Ananya Dubey (Annie1007) <ananyadubey1007@gmail.com>
2026-02-17 10:12:30 +0530

    feature/UC2: Render OOPS as Banner

---
+++ OOPSBannerUC2.java
@@ -0,0 +1,12 @@
+public class OOPSBannerUC2 {
+    public static void main(String[] args) {
+        // UC2: Large OOPS Banner using 7-line format and 9-character width
+        System.out.println("  ***** " + "   ***** " + "  ***** " + "  ***** ");
+        System.out.println(" *     * " + " *     * " + " *    * " + " *     ");
+        System.out.println(" *     * " + " *     * " + " *    * " + " *      ");
+        System.out.println(" *     * " + " *     * " + " ***** " + "  ***** ");
+        System.out.println(" *     * " + " *     * " + " * " + "          * ");
+        System.out.println(" *     * " + " *     * " + " * " + "          * ");
+        System.out.println("  ***** " + "   *****  "  + " * " + "      ***** ");
+    }
+}
